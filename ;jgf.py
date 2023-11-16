4f10e10448b8b98fc3c4b24dd33b13fe5b419e8b
ALIK SIVSEV <151023398+raznosperdaka@users.noreply.github.com>
2023-11-16 14:20:32 +0300

    Add files via upload

---
+++ ;jgf.py
@@ -0,0 +1,10 @@
+import os
+
+def print_docs (directory):
+    all_files = os.walk(directory)
+    for catalog in all_files:
+        print(f'Папка {catalog[0]} содержит:')
+        print(f'Директории{", ".join([folder for folder in catalog[1]])}')
+        print(f'Файлы: {", ".join([file for file in catalog[2]])}')
+        print('_' * 40)
+print_docs('C:/Users/user105/Desktop/123')
